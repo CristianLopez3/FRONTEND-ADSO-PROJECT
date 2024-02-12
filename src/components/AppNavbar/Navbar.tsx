26358648ab8536f4b920e1b774a0e554063d944a
Cristian Camilo Lopez <126030346+CristianLopez3@users.noreply.github.com>
2024-02-12 15:03:17 +0000

    update menu navbar with a better state and style

--- src/components/AppNavbar/Navbar.tsx
+++ src/components/AppNavbar/Navbar.tsx
@@ -5,12 +5,12 @@ import DarkMode from "./DarkMode";
 export default function Navbar() {
   return (
     <nav className="shadow-2xl bg-white dark:bg-gray-800 dark:text-white duration-200">
-      <div className="container py-3 sm:py-1 ">
+      <div className="container py-3 md:py-1 ">
         <div className="flex justify-between items-center">
           <div>
             <a
               href="#"
-              className="flex items-center justify-center gap-2 text-2xl sm:text-3xl font-bold"
+              className="flex items-center justify-center gap-2 text-2xl md:text-3xl font-bold"
             >
               <img src={Logo} alt="MenuEASY" className="w-10" />
               MenuEASY
@@ -22,7 +22,7 @@ export default function Navbar() {
               <DarkMode />
             </div>
 
-            <ul className=" hidden sm:flex gap-4">
+            <ul className=" hidden md:flex gap-4">
               <li>
                 <a
                   href="#"
@@ -48,7 +48,7 @@ export default function Navbar() {
                 </a>
               </li>
             </ul>
-            <button className="flex gap-2 bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 rounded-full hover:scale-105 duration-300 sm:hidden">
+            <button className="flex gap-2 bg-gradient-to-r from-primary to-third text-white px-4 py-2 rounded-full hover:scale-105 duration-300 md:hidden">
               Book
               <MdMenuBook className="text-xl text-white drop-shadow-sm cursor-pointer" />
             </button>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,30 @@
 import { type ReactNode, type ComponentPropsWithoutRef } from "react";
 
-
 const variantStyle = {
-  primary: "bg-primary text-black px-4 py-2 rounded hover:scale-105 duration-200",
-  secondary: "bg-primary text-white px-4 py-2 rounded hover:scale-105 duration-200"
-}
+  primary:
+    "bg-primary text-black px-4 py-2 rounded hover:scale-105 duration-200",
+  secondary:
+    "bg-primary text-white px-4 py-2 rounded hover:scale-105 duration-200",
+  black:
+    "bg-black text-white px-4 py-2 rounded hover:scale-105 duration-200 dark:border-white",
+  empty:
+    "border bg-transparent mx-2 text-black px-4 py-2 rounded duration-200 dark:text-white",
+};
 
 type ButtonProps = {
-  variant: 'primary' | 'secondary';
+  variant: "primary" | "secondary" | "black" | "empty";
   content: string | number;
   children?: ReactNode;
 } & ComponentPropsWithoutRef<"button">;
 
-
-export default function Button({variant, content, children, ...props}: ButtonProps) {
+export default function Button({
+  variant,
+  content,
+  children,
+  ...props
+}: ButtonProps) {
   return (
-    <button className={variantStyle[variant]} {...props} >
+    <button className={variantStyle[variant]} {...props}>
       {content}
       {children}
     </button>

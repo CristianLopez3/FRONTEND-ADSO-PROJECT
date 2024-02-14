b8a7922501adb5df1657acd474c44beae1b6bdd6
Cristian Camilo Lopez <126030346+CristianLopez3@users.noreply.github.com>
2024-02-14 12:36:47 +0000

    before intall keep

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./index.css";
 import Landing from "./pages/Landing";
+import Input from "./components/ui/Input";
 
 function App() {
   return (
     <div className="relative">
-      <Landing />
+      {/* <Landing /> */}
+      <div className="mt-10 ml-10">
+        <Input />
+      </div>
     </div>
   );
 }
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,21 @@
+export default function Input() {
+  return (
+    <>
+      <div className="relative mb-6" data-te-input-wrapper-init>
+        <input
+          type="text"
+          className="peer block min-h-[auto] w-full rounded border border-secondary bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+          id="exampleInput123"
+          aria-describedby="emailHelp123"
+          placeholder="First name"
+        />
+        <label
+          htmlFor="emailHelp123"
+          className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+        >
+          First name
+        </label>
+      </div>
+    </>
+  );
+}

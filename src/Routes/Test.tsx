77830ac72e2c779859b72407d60e78795f192ab0
Cristian <cristian.c.lopez.m@gmail.com>
2024-02-26 10:24:23 -0500

    use of useForm and zod

--- src/Routes/Test.tsx
+++ src/Routes/Test.tsx
@@ -1,19 +1,143 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { PiSpinnerGapLight } from "react-icons/pi";
+import { z } from "zod";
 
-import Sidebar, {SidebarItem} from '../components/layout/Sidebar/Sidebar'
-import { LuUserCircle } from "react-icons/lu";
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+// type formFileds = {
+//   email: string;
+//   password: string;
+// };
+
+type formFields = z.infer<typeof schema>;
 
 const Test = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<formFields>({
+    defaultValues: {
+      email: "test@email.com",
+      password: "************",
+    },
+    resolver: zodResolver(schema),
+  });
+  const onSubmit: SubmitHandler<formFields> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      throw new Error();
+      console.log(data);
+    } catch (errr) {
+      setError("email", {
+        message: "This email is already taken",
+      });
+    }
+  };
+
   return (
-    <main className='App'>
-      <Sidebar>
-        <SidebarItem path="/dashboard"  icon={<LuUserCircle size={20} />} text='Sser' active={false} alert  />
-        <SidebarItem path="/dashboard" icon={<LuUserCircle size={20} />} text='Menus' active={false} alert={false}  />
-        <SidebarItem path="/dashboard" icon={<LuUserCircle size={20} />} text='Reservations' active alert={false}  />
-        <SidebarItem path="/dashboard" icon={<LuUserCircle size={20} />} text='Events' active={false} alert={false} />
-      </Sidebar>
-
+    <main className="App flex justify-center items-center">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...register("email")}
+          type="text"
+          placeholder="Email"
+          className="block my-2 p-2"
+        />
+        {errors.email && (
+          <div className="text-red-500">{errors.email.message}</div>
+        )}
+        <input
+          {...register("password")}
+          type="password"
+          className=" block my-2 p-2"
+          placeholder="Password"
+        />
+        {errors.password && (
+          <div className="text-red-500">{errors.password.message}</div>
+        )}
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? (
+            <div>
+              <PiSpinnerGapLight className="animate-spin" /> is processing...
+            </div>
+          ) : (
+            "submit"
+          )}
+        </button>
+      </form>
     </main>
-  )
-}
+  );
+};
 
-export default Test
+export default Test;
+
+// ! WITHOUT ZOD
+// const Test = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     setError,
+//     formState: { errors, isSubmitting },
+//   } = useForm<formFileds>({defaultValues: {
+//     email: "test@email.com",
+//     password: "************"
+//   }});
+//   const onSubmit: SubmitHandler<formFileds> = async (data) => {
+//     try {
+//       await new Promise((resolve) => setTimeout(resolve, 1000));
+//       throw new Error();
+//       console.log(data);
+//     } catch(errr){
+//       setError("email", {
+//         message: "This email is already taken"
+//       })
+//     }
+
+//   };
+
+//   return (
+//     <main className="App flex justify-center items-center">
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <input
+//           {...register("email", { required: "email is required" })}
+//           type="text"
+//           placeholder="Email"
+//           className="block my-2 p-2"
+//         />
+//         {errors.email && (
+//           <div className="text-red-500">{errors.email.message}</div>
+//         )}
+//         <input
+//           {...register("password", {
+//             required: "Password is required",
+//             minLength: {
+//               value: 8,
+//               message: "Password must have at least 8 characters",
+//             },
+//           })}
+//           type="password"
+//           className=" block my-2 p-2"
+//           placeholder="Password"
+//         />
+//         {errors.password && (
+//           <div className="text-red-500">{errors.password.message}</div>
+//         )}
+//         <button type="submit" disabled={isSubmitting}>
+//           {isSubmitting ? (
+//             <div>
+//               <PiSpinnerGapLight className="animate-spin" /> is processing...
+//             </div>
+//           ) : (
+//             "submit"
+//           )}
+//         </button>
+//       </form>
+//     </main>
+//   );
+// };

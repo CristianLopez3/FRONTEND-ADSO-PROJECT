c97998ae101ea1217b48c8ad8b501a88f176b0dc
Cristian <cristian.c.lopez.m@gmail.com>
2024-03-02 15:12:59 -0500

    structure folder changed

--- src/components/menu/MenuTable.tsx
+++ src/components/menu/MenuTable.tsx
@@ -2,44 +2,45 @@ import { Menu } from "../../types/Menu";
 import Table from "../UI/Table";
 import MenuMobileItem from "./MenuMobileItem";
 import MenuRow from "./MenuRow";
-type MenuTableProps = {
-  data: Menu[]
-}
 
-const MenuTable = ({data}: MenuTableProps) => {
+type MenuTableProps = {
+  data: Menu[];
+};
+
+const MenuTable: React.FC<MenuTableProps> = ({ data }) => {
   return (
     <Table
-          data={data}
-          columns={[
-            { title: "ID", width: "10%" },
-            { title: "Title", width: "20%" },
-            { title: "Description", width: "30%" },
-            { title: "Price", width: "20%" },
-            { title: "Quantity", width: "10%" },
-            { title: "Actions", width: "10%" },
-          ]}
-          renderRowItems={(item: Menu, index) => (
-            <MenuRow
-              id={item.id}
-              description={item.description}
-              title={item.title}
-              price={item.price}
-              quantity={item.quantity}
-              key={index}
-            />
-          )}
-          renderMobileItems={(item: Menu, index) => (
-            <MenuMobileItem
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              description={item.description}
-              price={item.price}
-              quantity={item.quantity}
-            />
-          )}
+      data={data}
+      columns={[
+        { title: "ID", width: "10%" },
+        { title: "Title", width: "20%" },
+        { title: "Description", width: "30%" },
+        { title: "Price", width: "20%" },
+        { title: "Quantity", width: "10%" },
+        { title: "Actions", width: "10%" },
+      ]}
+      renderRowItems={(item: Menu, index) => (
+        <MenuRow
+          key={index}
+          id={item.id}
+          description={item.description}
+          title={item.title}
+          price={item.price}
+          quantity={item.quantity}
         />
-  )
-}
+      )}
+      renderMobileItems={(item: Menu, index) => (
+        <MenuMobileItem
+          key={index}
+          id={item.id}
+          title={item.title}
+          description={item.description}
+          price={item.price}
+          quantity={item.quantity}
+        />
+      )}
+    />
+  );
+};
 
 export default MenuTable;

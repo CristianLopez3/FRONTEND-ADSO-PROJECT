b64a9b5f733daa03a9a5b5460e06ccc5c61279df
cristian <cristian.c.lopez.m@gmail.com>
2024-04-10 22:30:06 -0500

    Start protecting routes

--- src/api/base.api.ts
+++ src/api/base.api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:9000/api/v1";
+const BASE_URL = "http://localhost:8080/api/v1/";
 
 export const instance = axios.create({
   baseURL: BASE_URL

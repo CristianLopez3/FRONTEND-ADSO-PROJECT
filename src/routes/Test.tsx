6b296d03798f798fee311694f477b66c120805bf
cristian <cristian.c.lopez.m@gmail.com>
2024-04-14 22:36:37 -0500

    Start developing pdfs

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -1,31 +1,30 @@
-import { InputCheck } from "@/components/Input";
-import Toggle from "@/components/Toggle";
-import { useState } from "react";
-import Cookies from "universal-cookie";
+import {
+  Document,
+  Text,
+  Page,
+  StyleSheet,
+  Image,
+  PDFDownloadLink,
+} from "@react-pdf/renderer";
 
-const Test: React.FC = () => {
-  const [checked, setChecked] = useState(false);
-
-  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-
-  const cookie =new Cookies();
-
-  const someFunction = () => {
-    cookie.set("test", "test");
-  }
-
-  const  console1 = () => {
-    console.log(cookie.get("test"));
-    
-  }
-
+const DocumentPdf = () => {
   return (
-    <div className="bg-white text-black flex p-12 items-center justify-center">
-      <button onClick={someFunction} className="bg-red-500 p-4"> Set Cookie </button>
-      <button onClick={console1} className="bg-red-500 p-4"> Console </button>
-    </div>
+    <Document>
+      <Page size="A4">
+        <Text>There is some text for our first pdf</Text>
+      </Page>
+    </Document>
+  );
+};
+
+const Test = () => {
+  return (
+    <>
+      <div className="bg-red-500 p-10">Test</div>
+      <PDFDownloadLink document={<DocumentPdf />} fileName="Table.pdf">
+        {({ loading }) => (loading ? "Loading document..." : "Download now!")}
+      </PDFDownloadLink>
+    </>
   );
 };
 

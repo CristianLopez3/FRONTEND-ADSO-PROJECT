d9ef23631c1616bdfe7a4c42a4219ec9ff23d2f1
cristian <cristian.c.lopez.m@gmail.com>
2024-04-14 21:26:26 -0500

    before monday 15th abril

--- src/main.tsx
+++ src/main.tsx
@@ -8,5 +8,5 @@ import { store } from "@/store/store.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <RouterProvider router={router} />
-  </Provider>
+  </Provider>,
 );
